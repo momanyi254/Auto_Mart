bec6e7f1e41dc164bc70250881a558f4d4a97474
momanyi254 <momanyihenry@hotmail.com>
2019-06-02 11:30:47 +0300

    Create user signin endpoint

--- Server/api/middleware/helper.js
+++ Server/api/middleware/helper.js
@@ -1,6 +1,17 @@
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 
-
+exports.comparePassword = (password, hPassword) => bcrypt.compareSync(password, hPassword);
 
 exports.hashPassword = (password) => bcrypt.hashSync(password, 10);
 
+exports.validation_error = (user, schema, res) => {
+
+	const { error } = Joi.validate(user, schema);
+
+	if (error) {
+		return res.status(400).json({
+			message: error.details[0].message
+		});
+	}
+}
--- Server/api/router/users.js
+++ Server/api/router/users.js
@@ -6,6 +6,7 @@ const userController = require('../controllers/users');
 
 router.post('/signup', userController.user_signup);
 
+router.post('/login', userController.user_signin);
 
 
 module.exports = router;
